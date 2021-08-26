39096f31507497c496d1f8b64135f196867c9476
illyanyc <illya.n@me.com>
2021-08-26 18:14:35 -0400

    modified all oracles

---
+++ oracle/oracle_googl.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENCED
+pragma solidity ^0.5.0;
+
+contract StockOracleGOOGL {
+    
+    /*storage*/
+    uint public close;
+    string public ticker = "GOOGL";
+    address public owner;
+
+    /*contructor*/
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    /*function setting price*/
+    function setClose(uint _close) public {
+        require(owner == msg.sender, "You are not the administrator of this oracle!");
+        close = _close;
+    }
+    
+    /*function getting the price*/
+    function getClose() public view returns (uint) {
+        return close;
+    }
+}
