2e59b7c75b2f91f0778b29d10f4d0150a5e82304
illyanyc <illya.n@me.com>
2021-08-31 22:17:37 -0400

    completed portfolio section

--- frontend/scripts/dapp.js
+++ frontend/scripts/dapp.js
@@ -1,5 +1,9 @@
 // Change this address to match your deployed contract!
 const contract_address = "0xeeE4afbBB163d6818e3e2251fcA26B0fe2298d65";
+// const contract_address = "0x228d0BA8395Df6109A080A034f6dc99A6c71A483";
+// const contract_address = "0x1383fC8D9644f734414D8B05E5d204eBe3F7a31b";
+// const contract_address = "0x7b272e6c2E049f49742631f7dfE8a724B3c030Db";
+
 
 var oracle_Contracts = {
 'AAPL':	'0x57960D9E1244deB9181BdC2a6B34968718fed1A4',
@@ -14,8 +18,15 @@ var oracle_Contracts = {
 var EthPrice;
 var stockPrice;
 var optionList;
+// var shortOptionList;
+// var longOptionList;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const dApp = {
+
   ethEnabled: function() {
     // If the browser has MetaMask installed
     if (window.ethereum) {
@@ -30,6 +41,7 @@ const dApp = {
   writeCallOption: async function(strike, premium, shares, expiry, tknAmt, ticker) {
     this.contract.methods.writeCallOption(strike, premium, shares, expiry, tknAmt, ticker).send({from: this.accounts[0], value : tknAmt}).on("receipt", (receipt) => {
       M.toast({ html: "Call option successfully written." });
+      this.updateAll();
       location.reload();
     });
   },
@@ -38,6 +50,25 @@ const dApp = {
   writePutOption: async function(strike, premium, shares, expiry, tknAmt, ticker) {
     this.contract.methods.writePutOption(strike, premium, shares, expiry, tknAmt, ticker).send({from: this.accounts[0], value : tknAmt}).on("receipt", (receipt) => {
       M.toast({ html: "Put option successfully written." });
+      this.updateAll();
+      location.reload();
+    });
+  },
+
+  // Buy Option
+  buyOption: async function(ID, premium) {
+    this.contract.methods.buyOption(ID).send({from: this.accounts[0], value : premium}).on("receipt", (receipt) => {
+      M.toast({ html: "Call option successfully bought." });
+      this.updateAll();
+      location.reload();
+    });
+  },
+
+  // Cancel Option
+  cancelOption: async function(ID) {
+    this.contract.methods.cancelOption(ID).send({from: this.accounts[0]}).on("receipt", (receipt) => {
+      M.toast({ html: "Call option successfully canceled." });
+      this.updateAll();
       location.reload();
     });
   },
@@ -69,22 +100,28 @@ const dApp = {
     return stockPrice/100
   },
 
+  // Get all options on the blokchain
   getAllAvailableOptions: async function() {
-    this.contract.methods.getAllAvailableOptions().call().then((response) => {
+    await this.contract.methods.getAllAvailableOptions().call().then((response) => {
         if (response && response.length > 0) {
-            // console.log({response});
-            // return {response};
             optionList = {response}['response'];
-            console.log(optionList)
         }
     });
   },
 
-  buyOption: async function(ID, premium) {
-    this.contract.methods.buyOption(ID).send({from: this.accounts[0], value : premium}).on("receipt", (receipt) => {
-      M.toast({ html: "Call option successfully bought." });
-      location.reload();
-    });
+  // Get all options written by user
+  getMyOptions: async function() {
+    return await this.contract.methods.getMyOptions().call({from: this.accounts[0]});
+  },
+
+  // Get all options bought by user
+  getOptionsBought: async function() {
+    return await this.contract.methods.getOptionsBought().call({from: this.accounts[0]});
+  },
+  
+  // Update all blockchain data
+  updateAll: async function() {
+    this.getAllAvailableOptions();
   },
 
   main: async function() {
@@ -94,10 +131,11 @@ const dApp = {
     }
 
     this.accounts = await window.web3.eth.getAccounts();
+    myAddress = this.accounts[0];
 
-    this.optionABI = await (await fetch("./option_abi.json")).json();
-    this.ethOracleABI = await (await fetch("./eth_oracle_abi.json")).json();
-    this.stockOracleABI = await (await fetch("./stock_oracle_abi.json")).json();
+    this.optionABI = await (await fetch("./json/option_abi.json")).json();
+    this.ethOracleABI = await (await fetch("./json/eth_oracle_abi.json")).json();
+    this.stockOracleABI = await (await fetch("./json/stock_oracle_abi.json")).json();
 
     this.contract = new window.web3.eth.Contract(
       this.optionABI,
@@ -112,8 +150,11 @@ const dApp = {
     );
 
     console.log("Contract object", this.contract);
-    this.getAllAvailableOptions();
+
+    // Get all data from blockchain
+    this.updateAll();
   }
 };
 
 dApp.main();
+// dApp.updateAll();
